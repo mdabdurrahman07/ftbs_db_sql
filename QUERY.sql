6baa2dfa760f6bd3f719a78ece6d025829d7c5f0
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-06-14 00:31:33 +0600

    query 1 done

--- QUERY.sql
+++ QUERY.sql
@@ -183,3 +183,15 @@ VALUES
   (504, 2, 101, NULL, NULL, 150.00),
   (505, 3, 102, 'C-20', 'Pending', 120.00);
 
+-- =========================================================================
+-- ALL QUERIES STARTS HERE
+-- =========================================================================
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+select * from matches where tournament_category = 'Champions League' and match_status = 'Available';
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
